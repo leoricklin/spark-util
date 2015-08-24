cf1f1f0a177a7a8d9cad703f52e1fe8c482b494d
leoricklin <leoricklin@gmail.com>
2015-08-24 02:29:16 +0000

    Refactoring NAStat.scala and NAStatCounter.scala to package tw.com.chttl.spark.mllib.util, adding new class TimeEvaluation.scala

---
+++ src/main/scala/tw/com/chttl/spark/mllib/util/NAStatCounter.scala
@@ -0,0 +1,38 @@
+package tw.com.chttl.spark.mllib.util
+
+import org.apache.spark.util.StatCounter
+
+/**
+ * Created by leorick on 2015/8/10.
+ */
+
+class NAStatCounter extends Serializable {
+  val stats: StatCounter = new StatCounter()
+  var missing: Long = 0
+  var sum = 0D
+  def add(x: Double): NAStatCounter = {
+    if (java.lang.Double.isNaN(x)) {
+      missing += 1
+    } else {
+      stats.merge(x)
+      sum += sum
+    }
+    this
+  }
+  def merge(other: NAStatCounter): NAStatCounter = {
+    stats.merge(other.stats)
+    missing += other.missing
+    sum += other.sum
+    this
+  }
+  override def toString = {
+    f"stats: + ${stats.toString} + Sum: ${sum} + NaN: + ${missing}"
+  }
+}
+
+object NAStatCounter extends Serializable {
+  def apply(x: Double) = new NAStatCounter().add(x)
+
+  def na2Zero(d: Double) = if (Double.NaN.equals(d)) 0.0 else d
+}
+
---
+++ src/main/scala/tw/com/chttl/spark/test/util/TimeEvaluation.scala
@@ -0,0 +1,13 @@
+package tw.com.chttl.spark.test.util
+
+/**
+ * Created by leo on 2015/8/24.
+ */
+object TimeEvaluation {
+  def time[A](f: => A) = {
+    val s = System.nanoTime
+    val ret = f
+    println("time: "+(System.nanoTime-s)/1e6+"ms")
+    ret
+  }
+}
